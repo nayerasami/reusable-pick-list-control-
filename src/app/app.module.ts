0ed349505ba6f912885bd2c8a1c733b5629c7956
nayera sami <nayerasamy19@gmail.com>
2024-08-29 16:59:48 +0200

    initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReusablePickListComponent } from './components/reusable-pick-list/reusable-pick-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReusablePickListComponent
   ],
   imports: [
     BrowserModule
